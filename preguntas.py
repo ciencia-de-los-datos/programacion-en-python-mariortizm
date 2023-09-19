15727acaf80d79adda5ee585e56c953aefa898cc
Mariana Ortiz <122634035+mariortizm@users.noreply.github.com>
2023-09-19 15:41:46 -0500

    Update preguntas.py
    LAB Programación básica en Python completo. Ya había subido los 5 primeros puntos anteriormente.

--- preguntas.py
+++ preguntas.py
@@ -196,7 +196,6 @@ def pregunta_05():
     max_values = {}
     min_values = {}
 
-    # Dividir las líneas de datos en filas
     rows = data.strip().split('\n')
 
     for row in rows:
@@ -214,10 +213,9 @@ def pregunta_05():
         else:
             min_values[letra] = valor_columna_2
 
-    # Convierte los diccionarios en una lista de tuplas
-    respuesta_05 = [(letra, max_values[letra], min_values[letra]) for letra in sorted(max_values)]
+    result = [(letra, max_values[letra], min_values[letra]) for letra in sorted(max_values)]
 
-    return respuesta_05
+    return result
 
 
 def pregunta_06():
@@ -242,7 +240,35 @@ def pregunta_06():
     ]
 
     """
-    return
+    clave_valores_min = {}
+    clave_valores_max = {}
+    import csv
+    with open('data.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            if row:
+                _, _, _, _, columna5 = row
+                entries = columna5.split(',')
+
+                for entry in entries:
+                    key, value = entry.split(':')
+                    value = int(value)
+
+                    if key in clave_valores_min:
+                        if value < clave_valores_min[key]:
+                            clave_valores_min[key] = value
+                        if value > clave_valores_max[key]:
+                            clave_valores_max[key] = value
+                    else:
+                        clave_valores_min[key] = value
+                        clave_valores_max[key] = value
+
+    resultado_06 = []
+    for key in sorted(clave_valores_min.keys()):
+        resultado_06.append((key, clave_valores_min[key], clave_valores_max[key]))
+
+    return resultado_06
+
 
 
 def pregunta_07():
@@ -266,7 +292,32 @@ def pregunta_07():
     ]
 
     """
-    return
+    import csv
+
+    with open("data.csv", mode='r', newline='') as file:
+        csv_reader = csv.reader(file)
+        data = "\n".join(["\t".join(row) for row in csv_reader])
+
+    values_dict = {}
+
+    rows = data.strip().split('\n')
+
+    for row in rows:
+        columns = row.split('\t')
+        valor_columna_2 = int(columns[1])
+        letra_columna_1 = columns[0]
+
+        if valor_columna_2 in values_dict:
+            values_dict[valor_columna_2].append(letra_columna_1)
+        else:
+            values_dict[valor_columna_2] = [letra_columna_1]
+
+    respuesta_07 = [(valor, letras) for valor, letras in values_dict.items()]
+
+    respuesta_07.sort()
+
+    return respuesta_07
+
 
 
 def pregunta_08():
@@ -291,7 +342,26 @@ def pregunta_08():
     ]
 
     """
-    return
+    import csv
+
+    values_dict = {}
+
+    with open("data.csv", mode='r', newline='') as file:
+        csv_reader = csv.reader(file, delimiter='\t')
+        next(csv_reader)  
+
+        for row in csv_reader:
+            valor_columna_2 = int(row[1])
+            letra_columna_1 = row[0]
+
+            if valor_columna_2 in values_dict:
+                values_dict[valor_columna_2].add(letra_columna_1)
+            else:
+                values_dict[valor_columna_2] = {letra_columna_1}
+
+    respuesta_08 = [(key, sorted(list(value))) for key, value in sorted(values_dict.items())]
+
+    return respuesta_08
 
 
 def pregunta_09():
@@ -314,7 +384,68 @@ def pregunta_09():
     }
 
     """
-    return
+    data = """E	1	1999-02-28	b,g,f	jjj:12,bbb:3,ddd:9,ggg:8,hhh:2
+    A	2	1999-10-28	a,f,c	ccc:2,ddd:0,aaa:3,hhh:9
+    B	5	1998-05-02	f,e,a,c	ddd:2,ggg:5,ccc:6,jjj:12
+    A	3	1999-08-28	a,b	hhh:9,iii:5,eee:7,bbb:1
+    C	6	1999-12-01	f,g,d,a	iii:6,ddd:5,eee:4,jjj:12
+    A	7	1998-07-28	c,d	bbb:2,hhh:0,ccc:4,fff:1,aaa:7
+    A	9	1997-02-28	g,d,a	aaa:5,fff:8,ddd:2,iii:0,jjj:7,ccc:1
+    B	1	1999-05-10	b,a	fff:3,hhh:1,ddd:2
+    E	2	1997-04-12	d,e,a,f	eee:4,ccc:5,iii:9,fff:7,ggg:6,bbb:2
+    B	3	1999-11-23	d,b,g,f	bbb:7,jjj:9,fff:5,iii:4,ggg:3,eee:3
+    C	7	1998-01-17	d,c,f,b	hhh:6,eee:4,iii:0,fff:2,jjj:12
+    C	5	1998-12-28	d,e,a,c	bbb:7,iii:6,ggg:9
+    D	3	1999-10-15	g,e,f,b	bbb:9,aaa:3,ccc:6,fff:4,eee:2
+    E	8	1998-11-01	c,f	aaa:8,ddd:5,jjj:12
+    B	9	1999-08-12	d,b	ccc:7,jjj:6,fff:7,ddd:3,aaa:2
+    D	8	1997-12-01	f,e	ccc:8,eee:6,bbb:9,ddd:3
+    E	3	1997-07-28	e,b,f	bbb:6,iii:3,hhh:5,fff:4,ggg:9,ddd:2
+    D	5	1998-08-12	g,a	hhh:4,jjj:5,ccc:9
+    E	8	1999-08-24	e,c,f,a	ccc:1,iii:6,fff:9
+    E	9	1998-01-23	e,a	bbb:9,aaa:3,fff:1
+    E	7	1999-06-22	e,f	ddd:9,iii:2,aaa:4
+    E	3	1999-04-24	c,b,g	ccc:5,fff:8,iii:7
+    D	5	1999-06-25	c,f,a	eee:3,jjj:17,ddd:7
+    A	9	1999-08-25	f,a,d	jjj:12,ggg:7,ccc:7,ddd:9,bbb:3
+    E	4	1997-07-26	c,d	jjj:6,ccc:4,aaa:1,hhh:9,iii:7,ggg:8
+    E	6	1997-09-24	e,d,c	fff:3,eee:6,iii:4,bbb:7,ddd:4,ccc:1
+    A	8	1997-09-28	a,e,f	fff:0,ddd:5,ccc:4
+    E	5	1999-06-22	c,a,g	ggg:6,hhh:3,ddd:9,ccc:10,jjj:7
+    A	6	1999-07-29	f,e	hhh:6,jjj:13,eee:5,iii:7,ccc:3
+    C	0	1999-08-22	f,c,a,g	eee:1,fff:4,aaa:2,ccc:7,ggg:10,ddd:6
+    A	9	1998-04-26	b,f	ccc:6,aaa:9,eee:5,ddd:0,bbb:3
+    D	3	1998-02-24	b,f	bbb:7,hhh:1,aaa:6,iii:4,fff:9,ddd:5
+    E	5	1999-03-24	a,c	fff:3,ccc:1,ggg:3,eee:5
+    B	4	1998-03-23	b,f,c	iii:7,ggg:3,ddd:0,jjj:8,hhh:5,ccc:1
+    B	6	1999-04-21	f,a,e	hhh:6,ccc:3,jjj:9,bbb:8,ddd:7
+    D	7	1999-02-29	a,f	aaa:1,fff:5,ddd:3
+    B	8	1997-05-21	c,a	ddd:5,jjj:17,iii:7,ccc:10,bbb:4
+    C	9	1997-07-22	c,a,e,f	eee:3,fff:2,hhh:6
+    E	1	1999-09-28	e,d	fff:9,iii:2,eee:5
+    E	5	1998-01-26	f,a,d	hhh:8,ggg:3,jjj:5"""
+
+    clave_contador = {}
+
+    lines = data.strip().split('\n')
+
+    for line in lines:
+        parts = line.strip().split('\t')
+        column_5 = parts[4].split(',')
+
+        for entry in column_5:
+            key, _ = entry.split(':')
+            key = key.strip()  
+            key = key.strip('\ufeff') 
+            if key in clave_contador:
+                clave_contador[key] += 1
+            else:
+                clave_contador[key] = 1
+
+    # Ordenar el diccionario
+    clave_contador_ordenado = {k: v for k, v in sorted(clave_contador.items())}
+
+    return clave_contador_ordenado
 
 
 def pregunta_10():
@@ -335,7 +466,22 @@ def pregunta_10():
 
 
     """
-    return
+    respuesta_10 = []
+    import csv
+    with open('data.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        
+        for row in csvreader:
+            letra_columna_1 = row[0]
+            column_4 = row[3].split(',')
+            column_5 = row[4].split(',')
+
+            cantidad_columna_4 = len(column_4)
+            cantidad_columna_5 = len(column_5)
+
+            respuesta_10.append((letra_columna_1, cantidad_columna_4, cantidad_columna_5))
+
+    return respuesta_10
 
 
 def pregunta_11():
@@ -356,7 +502,26 @@ def pregunta_11():
 
 
     """
-    return
+    suma_letras = {}
+    import csv
+    with open('data.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        
+        for row in csvreader:
+            columna_2 = int(row[1])
+            letras_columna_4 = row[3].split(',')
+
+            for letra in letras_columna_4:
+                letra = letra.strip()  
+                letra = letra.strip('\ufeff')  
+                if letra in suma_letras:
+                    suma_letras[letra] += columna_2
+                else:
+                    suma_letras[letra] = columna_2
+
+    respuesta_11 = {k: v for k, v in sorted(suma_letras.items())}
+
+    return respuesta_11
 
 
 def pregunta_12():
@@ -374,4 +539,25 @@ def pregunta_12():
     }
 
     """
-    return
+    suma_columna_5 = {}
+    import csv
+    with open('data.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        
+        for row in csvreader:
+            letra_columna_1 = row[0]
+            column_5 = row[4].split(',')
+
+            for entry in column_5:
+                _, valor = entry.split(':')
+                valor = int(valor)
+                
+                if letra_columna_1 in suma_columna_5:
+                    suma_columna_5[letra_columna_1] += valor
+                else:
+                    suma_columna_5[letra_columna_1] = valor
+
+    # Ordenar el diccionario
+    respuesta_12 = dict(sorted(suma_columna_5.items()))
+
+    return respuesta_12
